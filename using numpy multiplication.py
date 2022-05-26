903f6668e1e16ad6b93a31455dfa68c80804d201
RAMCHANDRAPAL <106179157+RAMCHANDRAPAL@users.noreply.github.com>
2022-05-26 20:37:07 +0530

    Create using numpy multiplication.py

---
+++ using numpy multiplication.py
@@ -0,0 +1,9 @@
+import numpy as geek
+in_num1 = 4
+in_num2 = 6
+  
+print ("1st Input  number : ", in_num1)
+print ("2nd Input  number : ", in_num2)
+    
+out_num = geek.multiply(in_num1, in_num2) 
+print ("output number : ", out_num) 
