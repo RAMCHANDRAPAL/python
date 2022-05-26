bf9c37121aae431bbd8af9f640e65bf54b5f5f8f
RAMCHANDRAPAL <106179157+RAMCHANDRAPAL@users.noreply.github.com>
2022-05-26 23:02:44 +0530

    Create check character is lowercase or uppercase.py

---
+++ check character is lowercase or uppercase.py
@@ -0,0 +1,11 @@
+ch = input("Enter any character : ")[0]
+
+# Check for uppercase, lowercase
+if ch.isupper() :
+    print("\n" + ch, "is UPPERCASE alphabet.")
+
+elif ch.islower() :
+    print("\n" + ch, "is LOWERCASE alphabet.")
+
+else :
+    print("\n" + ch, "is not an alphabet.")
