8c4bfd34e437c3dab980cfd98c038a8584915acf
RAMCHANDRAPAL <106179157+RAMCHANDRAPAL@users.noreply.github.com>
2022-05-26 23:01:17 +0530

    Create calculate simple intrest.py

---
+++ calculate simple intrest.py
@@ -0,0 +1,5 @@
+principal = float(input("Enter principal : "))
+rate = float(input("\nEnter rate : "))
+time = float(input("\nEnter time (year) : "))
+result = (principal * rate * time) / 100
+print("\nSimple interest :", result)
