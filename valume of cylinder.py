97ef4bd25569b9fb283ce64fc03edb7dd2a07351
RAMCHANDRAPAL <106179157+RAMCHANDRAPAL@users.noreply.github.com>
2022-05-26 22:50:19 +0530

    Create valume of cylinder.py

---
+++ valume of cylinder.py
@@ -0,0 +1,6 @@
+pi = 3.14
+radius = int(input('Enter the radius of a cylinder: '))
+height = int(input('Enter the height of a cylinder: '))
+#Formula for Volume of Cylinder
+volume = pi * radius * radius * height
+print(" Volume of a cylinder: " ,volume)
