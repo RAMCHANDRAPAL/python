d1775d5721f51666a33761ca17b93dc995cc45ec
RAMCHANDRAPAL <106179157+RAMCHANDRAPAL@users.noreply.github.com>
2022-05-26 20:09:31 +0530

    Create find factorial any number.py

---
+++ find factorial any number.py
@@ -0,0 +1,13 @@
+def factorial(num):
+    
+    fact=1;
+    i=1
+    while i<=num:
+        fact=fact*i
+        i=i+1
+    return fact
+    
+num=input("Enter a number..");
+result=factorial(num)
+print("factorial of the number: %d" %result)
+factorial(num)
